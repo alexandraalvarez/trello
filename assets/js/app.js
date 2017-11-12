cfaec01fc97d50fc30971aad36d07cbb3001945b
alexandraalvarez <alexandra.alvarez.a@gmail.com>
2017-11-11 22:40:40 -0300

    Un avance importante

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 /*var listCreator = document.getElementByClassName("list-creator");*/
 document.getElementsByClassName("list-creator")[0].addEventListener("focus", function(){  /*para que se active al ponerle click*/
 	var buttonListCreator = document.createElement('button');/*para crear el botón*/
-	buttonListCreator.setAttribute("id","listCreator");/*para darle id al botón*/
+	buttonListCreator.setAttribute("id","buttonCreator");/*para darle id al botón*/
 	buttonListCreator.setAttribute("type","button");/*para recordarle que es un tipo botón*/	
 
 	var divCreator = document.getElementById("div-creator");/*enlazando el botón a su div padre*/
@@ -12,34 +12,44 @@ document.getElementsByClassName("list-creator")[0].addEventListener("focus", fun
 	divCreator.appendChild(buttonListCreator);
 
 
-	/*tiene que aparecer un div nuevo con el nombre que puso el usuario en el input del html*/
-	var listsContainer = document.getElementById("lists-container"); /*para darle un padre al div que tendrá las listas*/
-	var lists = document.createElement("div"); /*para guardar cada lista nueva*/
-	lists.setAttribute("class","lists"); /*para asignar una clase a cada lista que se cree*/
-	listsContainer.appendChild(lists); /*para darle padre a cada lista que se cree*/
 
-	/*crear un título para la lista y rescatarlo del input original al hacer click en guardar*/
-	var titleList = document.createElement("h4"); /*título de la lista nueva*/
-	var textTitleList = document.createTextNode("");
-	titleList.appendChild(textTitleList); /*para que el nodo texto se una al h4*/
-	lists.appendChild(titleList); /*para poner el título de la lista dentro del div lista*/
+
+	
 
 	buttonListCreator.addEventListener("click", function(){ /*llamo al botón creado arriba y le agrego evento*/
+		/*tiene que aparecer un div nuevo con el nombre que puso el usuario en el input del html*/
+		var listsContainer = document.getElementById("lists-container"); /*para darle un padre al div que tendrá las listas*/
+		var lists = document.createElement("div"); /*para guardar cada lista nueva*/
+		lists.setAttribute("class","lists"); /*para asignar una clase a cada lista que se cree*/
+		listsContainer.appendChild(lists); /*para darle padre a cada lista que se cree*/
+
+		/*crear un título para la lista y rescatarlo del input original al hacer click en guardar*/
+		var titleList = document.createElement("p"); /*título de la lista nueva*/
+		var textTitleList = document.createTextNode("");
+		titleList.appendChild(textTitleList); /*para que el nodo texto se una al h4*/
+		lists.appendChild(titleList); /*para poner el título de la lista dentro del div lista*/
+		
 		var newTitle = document.getElementsByClassName('list-creator')[0].value;//aca con .value recupero lo que ingresó el usuario, para luego utilizarlo
 		document.getElementsByClassName('list-creator')[0].value = "";//aca le indico que una vez ya rescatado lo ingresado, quede limpio nuevamente mi input	
-		textTitleList.value = newTitle;
+		textTitleList.textContent = newTitle;
+
+		/*input para la lista de tareas*/
+		var listHomework = document.createElement("input"); /*crear un div para la lista de tareas*/
+		listHomework.setAttribute("type","text");/*input tipo texto*/
+		listHomework.setAttribute("placeholder","Agrega una tarea"); /*dirá agregar tareas*/
+		listHomework.setAttribute("class","inputLists")/*clase al input para poder llamarlo*/
+		lists.appendChild(listHomework);
 
 		})
 	})
 		
 	
-	/*input para la lista de tareas*/
-	/*var listHomework = document.createElement("input"); /*crear un div para la lista de tareas*/
-	/*listHomework.setAttribute("type","text");/*input tipo texto*/
-	/*listHomework.setAttribute("placeholder","Agrega una tarea"); /*dirá agregar tareas*/
-	/*listHomework.classList.add("class","inputLists")/*clase al input para poder llamarlo*/
+	
 
 	
 	
 	
 	/*})*/
+
+/*puedo llamar a función insertBefore para que las listas aparezcan antes del div creator*/
+/*necesito desactivar el focus para que dejen de aparecer botones guardar*/
